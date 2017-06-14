e62315ec5f4855f75c20d06e5fb01806fefa664f
sebtn <sdebedout@gmail.com>
2017-06-14 00:11:36 -0400

    Firs commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Karma ready testing boiler plate 
+-----
++ Uses webpack and express to serve devEnv
++ Node js and react for UI designing
++ Bootstrapped for components styling 
+
+* **[Sebtn]** 
---
+++ app/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+import {Route, Router, IndexRoute, hashHistory, Link} from 'react-router'
+import  '././styles/app.scss' 
+import './redux-example.js'
+
+'use strict'
+
+ReactDOM.render(<h3>Using Redux</h3>, document.getElementById('root'))
