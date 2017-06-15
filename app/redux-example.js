b41287a0f11043158125a03ca3510ce6973a8c40
sebtn <sdebedout@gmail.com>
2017-06-15 01:22:56 -0400

    Added map reducer, async capable state set

--- app/redux-example.js
+++ app/redux-example.js
@@ -1,8 +1,9 @@
+import axios from 'axios'
 let redux = require('redux')
 
 'use strict'
 /*------------------------------------------------------*/
-let nameReducer = (state= 'Anonymous', action) => {
+let nameReducer = (state = 'Anonymous', action) => {
 	switch (action.type) {
 		case "CHANGE_NAME":
 			/*No spread operator because only manages one prop*/
@@ -61,7 +62,6 @@ let removeHobby = (id) => {
 	}
 }
 
-
 /*------------------------------------------------------*/
 let nextMovieId = 1 
 let moviesReducer = (state = [], action ) => {
@@ -79,6 +79,7 @@ let moviesReducer = (state = [], action ) => {
 				genre: action.genre
 			}
 		]
+/*------------------------------------------------------*/
 		case "REMOVE_MOVIE":
 			return state.filter( (movie) => movie.id !== action.id )
 /*------------------------------------------------------*/
@@ -104,51 +105,108 @@ let removeMovie = (id) => {
 }
 
 /*------------------------------------------------------*/
+let mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+	switch (action.type) {
+/*------------------------------------------------------*/
+		case "START_LOCATION_FETCH":
+			return {
+				isFetching: true,
+				url: undefined
+			}
+/*------------------------------------------------------*/
+		case "COMPLETE_LOCATION_FETCH":
+			return {
+				isFetching: false,
+				url: action.url
+			}
+/*------------------------------------------------------*/
+		default: 
+			return state						
+	}		
+}
+
+/*Generators*/
+/*------------------------------------------------------*/
+let startLocationFetch = () => {
+	return {
+		type: "START_LOCATION_FETCH"
+	}
+}
+
+/*------------------------------------------------------*/
+let completeLocationFetch = (url) => {
+	return {
+		type: "COMPLETE_LOCATION_FETCH",
+		url
+	}
+}
+
+/*------------------------------------------------------*/
+let fetchLocation = () => {
+	store.dispatch(startLocationFetch())
+
+	axios.get("http://ipinfo.io").then( function (res)  {
+		let location = res.data.loc
+		let baseUrl = "https://maps.google.com?q="
+
+		store.dispatch(completeLocationFetch(baseUrl + location))
+	})
+}
+ 
+/*------------------------------------------------------*/
+/*Combine reducers*/
 let reducer = redux.combineReducers({
-	/*Manage name with name reducer*/
+	/*Manage name with nameReducer*/
 	name: nameReducer,
+	/*Manage hobbies with hobbiesReducer*/
 	hobbies: hobbiesReducer,
-	movies: moviesReducer
+	/*Manage movies with moviesReducer*/
+	movies: moviesReducer, 
+	/*Manage map with mapReducer*/
+	map: mapReducer
 })
 
 /*------------------------------------------------------*/
-/*Middleware = redux.compose*/
+/*Middleware = add arg redux.compose*/
 let store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f 
 ))
 
 /*------------------------------------------------------*/
-/*Subscribe to changes to watch changes state using a cb
-state.name will contain both dispatches made */
+/*Subscribe to changes to watch changes state using a call back 
+will contain all dispatches made */
 let unsubscribe =  store.subscribe( () => {
 	let state = store.getState()
 
-	document.getElementById('root').innerHTML = state.name
+	// document.getElementById('root').innerHTML = state.name
+	console.log('State Evolving:  ', store.getState())
 
-	console.log('Did something changed? ', store.getState())
+	if (state.map.isFetching) {
+		return document.getElementById('root').innerHTML = 'Loading ...'
+	} else if (state.map.url) {
+		return document.getElementById('root').innerHTML = 
+			'<a href=" '+ state.map.url + '" target="blank"> View Location in maps app</a>'	
+	}
+
 })
 /* unsubscribe() => use it to reset the subscriptions */ 
 
 /*------------------------------------------------------*/
+fetchLocation()
+
+/*------------------------------------------------------*/
 let currentState = store.getState()
-	console.log('name is : ', currentState)
+	// console.log('name is : ', currentState)
 
 /*------------------------------------------------------*/
 /*Cb dispatched into subscribe, cb are actions*/
 store.dispatch(changeName("Seb is now the name"))
-
 store.dispatch(addHobby( "Build machines" ))
-
 store.dispatch(addHobby( "Not run" ))
-
 store.dispatch(addMovie( "Favorite movie numero 1", "terror"))
-
 store.dispatch(changeName("Seb is just awesome"))
-
 store.dispatch(addHobby( "Build helicopters" ))
-
 store.dispatch(addMovie( "Batman 2", "ficion asion" ))
-
 /*remove hobby*/
 store.dispatch(removeHobby(2))
 /*Remove movie*/
